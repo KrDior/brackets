9fa0dd8682593bdaea26b5ada8233e41ee6f8121
KrDior <xzx89s@gmail.com>
2019-03-10 14:54:10 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
+/*module.exports = */
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let stack = [];
+    let i, bracePosition;
+    let checkRep = [];
+    bracketsConfig = bracketsConfig.join().replace(/,/g, '');
+    for (i = 0; i < str.length; i++) {
+        bracePosition = bracketsConfig.indexOf(str[i]);
+        if (bracePosition === -1 || str.length%2 != 0) {
+            return false;
+        }
+        
+        if (bracePosition % 2 === 0) {
+            if (bracketsConfig[bracePosition+1] == bracketsConfig[bracePosition] && checkRep.indexOf(bracketsConfig[bracePosition]) == -1) {
+                stack.push(bracePosition + 1);
+                checkRep.push(bracketsConfig[bracePosition]);
+            } else if (bracketsConfig[bracePosition+1] == bracketsConfig[bracePosition] && checkRep.indexOf(bracketsConfig[bracePosition]) !== -1) {
+                checkRep.pop(bracketsConfig[bracePosition]);
+                stack.pop()
+            } else {
+                stack.push(bracePosition + 1);
+            }
+
+        } else if (stack.pop() !== bracePosition) {
+            return false;
+        }
+    }
+    return stack.length === 0 ? true : false;
 }
